7077d23bafcffb65515789ea1516af82c274e2d9
Serge <villasica.serge@gmail.com>
2019-07-04 10:21:43 +0800

    Add Quote Screen

---
+++ app/src/main/java/com/serge/moneyme/QuoteActivity.kt
@@ -0,0 +1,12 @@
+package com.serge.moneyme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QuoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quote)
+    }
+}
