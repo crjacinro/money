4dbccb285dc2fd7c50291632f3df549d64b009f3
Serge <villasica.serge@gmail.com>
2019-07-04 17:00:51 +0800

    Add PMT helper

---
+++ app/src/main/java/com/serge/moneyme/service/PMTHelper.kt
@@ -0,0 +1,18 @@
+package com.serge.moneyme.service
+
+import kotlin.math.pow
+
+
+class PMTHelper {
+    companion object {
+        fun calculatePMT(R: Double, n: Double, Pv: Double): Double {
+            val pvTimesR = (Pv * R)
+            val onePlusR = 1 + R
+            val negativeN = n * -1
+
+            val denominator = (1 - (onePlusR.pow(negativeN)))
+
+            return pvTimesR / denominator
+        }
+    }
+}
